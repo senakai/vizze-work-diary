b835457b2cc65e8d4e59d1ec2f487ad75780f136
senakai <gabriel.abrego.sena@gmail.com>
2021-09-15 17:38:08 -0400

    server configured, links updated

--- src/js/form.js
+++ src/js/form.js
@@ -11,3 +11,14 @@ form.forEach((item, i) => {
     }, i*100);
 
 })
+
+//validation 
+
+const name = document.querySelector('.name') || null;
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const password = document.querySelector('.submit-btn');
+
+if(name == null){ //means login page is open
+
+}
