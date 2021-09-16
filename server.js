d970f9718886ef62c310b4d9ed9b4df1248bdcc2
senakai <gabriel.abrego.sena@gmail.com>
2021-09-16 17:23:44 -0400

    form validation

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const path = require('path');
 // const bodyParser = require('body-parser');
 const knex = require('knex');
 
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1', 
+        user: 'postgres',
+        password: 'leostronda123',
+        database: 'loginformdiario'
+    }
+})
+
 const app = express();
 
 let initialPath = path.join(__dirname, "src");
@@ -25,6 +35,30 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(initialPath, "register.html"));
 })
 
+app.post('/register-user', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if(!name.length || !email.length || !password.length){
+        res.json('fill all the fields');
+    } else {
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if(err.detail.includes('already exists')){
+                res.json('email already exists');
+            }
+        })
+    }
+})
+
 app.listen(3000, (req, res) => {
     console.log('listening on port 3000........')
 })
+
--- src/js/form.js
+++ src/js/form.js
@@ -12,13 +12,45 @@ form.forEach((item, i) => {
 
 })
 
+
 //validation 
 
-const name = document.querySelector('.name') || null;
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const password = document.querySelector('.submit-btn');
+// !(function severalHoursLater(){
 
-if(name == null){ //means login page is open
+    const name = document.querySelector('.name') || null;
+    const email = document.querySelector('.email');
+    const password = document.querySelector('.password');
+    const submitBtn = document.querySelector('.submit-btn');
+    
+    
+    
+    if(name == null){ //means login page is open
+    
+    
+    
+    } else { 
+    
+        submitBtn.addEventListener('click', () => {
+            fetch('/register-user', {
+                method: 'post',
+                headers: new Headers({'Content-Type': 'application/json'}),
+                body: JSON.stringify({
+                    name: name.value,
+                    email: email.value,
+                    password: password.value
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.name){
+                    alert('register successful');
+                } else {
+                    alert(data);
+                }
+            })
+        })
+    }
 
-}
+// })
+
+// severalHoursLater();
